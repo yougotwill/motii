e91a59b8a336082e0a4dcc479932e420ccdae38e
William Grant <willmgrant@gmail.com>
2020-10-07 05:25:37 +0000

    added img alt

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -135,7 +135,7 @@ const Calendar = ({
       </Modal>
       <Modal isOpen={isSuccessModalOpen} closeHandler={() => { handleModal(isSuccessModalOpen, setSuccessModalOpen); }} shake={true}>
         <h4>Great job!</h4>
-        <img src='https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg' />
+        <img src='https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg' alt='cat' />
         <p>Nice work! Let's keep up the habit.</p>
       </Modal>
     </div>
