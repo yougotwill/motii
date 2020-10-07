6d4c6d6f9893ab6eb9095ff5f3467a6b7c9eeaac
William Grant <willmgrant@gmail.com>
2020-10-07 05:21:21 +0000

    added confetti and shaking success modal

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,8 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import confetti from 'canvas-confetti';
 
 import { MONTHS, DAYS_OF_WEEK, getNumDays } from '../../shared/datetime';
 
 import Modal from '../Modal';
+
+const runConfetti = () => {
+  const duration = 3 * 1000;
+  const animationEnd = Date.now() + duration;
+  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 100 };
+  
+  const randomInRange = (min, max) => {
+    return Math.random() * (max - min) + min;
+  };
+  
+  const interval = setInterval(() => {
+    const timeLeft = animationEnd - Date.now();
+  
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+  
+    const particleCount = 250 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+  }, 250);
+};
 
 const Calendar = ({
   handleConfigChange,
@@ -14,7 +38,8 @@ const Calendar = ({
   updateMissedDays,
   handleModal
 }) => {
-  const [isModalOpen, setModalOpen] = useState(false);
+  const [isWarningModalOpen, setWarningModalOpen] = useState(false);
+  const [isSuccessModalOpen, setSuccessModalOpen] = useState(false);
 
   const getStartDayOfMonth = (date) => {
     return new Date(date.getFullYear(), date.getMonth(), 1).getDay();
@@ -48,16 +73,20 @@ const Calendar = ({
       if (habit.length > 0) {
         handleConfigChange(`data.${dateString}`, habit);
         value = 1;
+        
+        event.target.classList.toggle('success');
+        setDate(new Date(year, month, event.target.innerText));
+
+        handleModal(isSuccessModalOpen, setSuccessModalOpen);
+        // positive reinforcement
+        runConfetti();
+
+        if (month === today.getMonth()) {
+          updateStreak(streak += value);
+        }
       } else {
-        handleModal(isModalOpen, setModalOpen);
+        handleModal(isWarningModalOpen, setWarningModalOpen);
       }
-    }
-
-    event.target.classList.toggle('success');
-    setDate(new Date(year, month, event.target.innerText));
-
-    if (month === today.getMonth()) {
-      updateStreak(streak += value);
     }
   };
 
@@ -101,8 +130,13 @@ const Calendar = ({
             );
           })}
       </div>
-      <Modal isOpen={isModalOpen} closeHandler={() => { handleModal(isModalOpen, setModalOpen); }}>
+      <Modal isOpen={isWarningModalOpen} closeHandler={() => { handleModal(isWarningModalOpen, setWarningModalOpen); }}>
         <h4>Please fill in your habit.</h4>
+      </Modal>
+      <Modal isOpen={isSuccessModalOpen} closeHandler={() => { handleModal(isSuccessModalOpen, setSuccessModalOpen); }} shake={true}>
+        <h4>Great job!</h4>
+        <img src='https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg' />
+        <p>Nice work! Let's keep up the habit.</p>
       </Modal>
     </div>
   );
